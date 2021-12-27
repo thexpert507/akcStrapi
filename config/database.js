651fed57a388eff50cd0f8b5a051e8a88eabcadb
thexpert507 <66019912+thexpert507@users.noreply.github.com>
2021-12-26 19:31:36 -0500

    config mysql

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', 'ec2-54-235-79-88.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'akcStrapi'),
-      user: env('DATABASE_USERNAME', 'oroywksbzqcbuo'),
-      password: env('DATABASE_PASSWORD', '8c5fdb64dce20da7936659c5472389b95d25a49a1ba597fc9547a515a2ee6b41'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env("DATABASE_HOST", "161.35.63.16"),
+      port: env.int("DATABASE_PORT", 32852),
+      database: env("DATABASE_NAME", "akcStrapi"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "aka2100"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
